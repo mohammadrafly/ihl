d20ccb6e0faaa2bdab9dd0a272cc1de2bbac0cb1
Mohammad Rafly <mohammadrafly19@gmail.com>
2024-07-30 07:48:27 +0700

    revisi #2

--- resources/js/pages/layouts/partials/home/Header.jsx
+++ resources/js/pages/layouts/partials/home/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
                 </div>
             </div>
             <nav className="bg-green-500 text-gray-100 p-2 px-10 2xl:px-32 sticky top-0">
-                <div className="flex justify-between items-center py-1">
+                <div className="flex justify-between items-center">
                     <div className="hidden md:flex">
                         <ul className="flex space-x-5 uppercase font-semibold">
                             <li>
@@ -49,7 +49,7 @@ export default function Header() {
                             </li>
                         </ul>
                     </div>
-                    <div className="md:hidden">
+                    <div className="md:hidden flex justify-center items-center">
                         <button onClick={toggleMenu} className="focus:outline-none">
                             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
